46abefd49d8e2a70cfa91e094209d3eaa8392daa
Welpy-cw <h.w.forum@arcor.de>
2021-08-23 19:37:56 +0200

    Updated and simplified listener for renamed folders

--- chrome/content/folderAccount.js
+++ chrome/content/folderAccount.js
@@ -1,4 +1,8 @@
-
+ChromeUtils.defineModuleGetter(
+  this,
+  "MailServices",
+  "resource:///modules/MailServices.jsm"
+);
 
 var folderAccount = {
 
@@ -6,63 +10,11 @@ var folderAccount = {
     // Global var
     prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch),
   
-    folderListener: {
-        OnItemAdded: function(parentItem, item) { },
-
-
-        OnItemRemoved: function(parentItem, item) {
-
-            // Delete preference
-
-            if (item instanceof Components.interfaces.nsIMsgFolder) {
-
-                // Save this URI in case this turns out to be a rename...
-                folderAccount.folderListener.oldURI = item.URI; 
-            }
-
-            //folderAccount.logMsg('OnItemRemoved (delete), item>' + item + '< parentItem>' + parentItem + '<');
-        },
-
-        
-        
-        OnItemUnicharPropertyChanged: function(item, property, oldValue, newValue){
-          if (item instanceof Components.interfaces.nsIMsgFolder && property == "Name") {
-          
-              // This looks like a rename, so save the URI 
-              folderAccount.folderListener.newURI = item.URI;
-          }
-
-          //folderAccount.logMsg('OnItemUnicharPropertyChanged, item>' + item + '< property>' + property + '< oldValue>' + oldValue + '< newValue>' + newValue + '<');
-        },
-
-        
-
-        OnItemEvent: function (folder, event) {
-            
-            var eventType = event.toString();
-            
-            if (eventType == "RenameCompleted") {
-            
-                // It was indeed a rename, so let's update our settings
-                folderAccount.folderRenamed(folderAccount.folderListener.oldURI, folderAccount.folderListener.newURI);
-                
-                // And log it
-                //folderAccount.logMsg("Folder >" + folderAccount.folderListener.oldURI + "< renamed to >" +folderAccount.folderListener.newURI + "<");
-            }
-            
-            
-            folderAccount.folderListener.newURI = "";
-            folderAccount.folderListener.oldURI = "";
-
-            //folderAccount.logMsg('folderListener, event>' + eventType + '<');
-        },
-        
-        // Other folder events that we can ignore for now
-        OnItemPropertyChanged:      function(item, property, oldValue, newValue) { },    
-        OnItemIntPropertyChanged:   function(item, property, oldValue, newValue) { },
-        OnItemBoolPropertyChanged:  function(item, property, oldValue, newValue) { },
-        OnItemPropertyFlagChanged:  function(item, property, oldFlag, newFlag) { }
-    },
+  folderListener: {
+    folderRenamed(oldFolder, newFolder) {
+      folderAccount.folderRenamed(oldFolder.URI, newFolder.URI);
+    }
+  },
   
   
     // The following is run when a folder is renamed
@@ -164,9 +116,8 @@ var folderAccount = {
     // Add a listner to watch for a renamed folder
     // When a folder is renamed, we need to update our stored prefs to reflect the change
 
-    Components.classes["@mozilla.org/messenger/services/session;1"].
-        getService(Components.interfaces.nsIMsgMailSession).
-        AddFolderListener(folderAccount.folderListener, Components.interfaces.nsIFolderListener.all);
+      // nsIMsgFolderListener
+      MailServices.mfn.addListener(folderAccount.folderListener, MailServices.mfn.folderRenamed);
     },
    
 
@@ -176,37 +127,12 @@ var folderAccount = {
 
       // Remove our folder listner.  Is this stricly necessary??
 
-      Components.classes["@mozilla.org/messenger/services/session;1"].
-          getService(Components.interfaces.nsIMsgMailSession).
-          RemoveFolderListener(folderAccount.folderListener);
-    },
-
-
-    /////////////////////////////////////
-    // Administrative functions
-    /////////////////////////////////////
-
-    logMsg: function(txt) {
-        try {
-
-            // If debug preference is not present, this function will throw an error, which will be harmlessly caught
-            if (folderAccount.prefs.getBoolPref("extensions.folderaccount.debug")) {
-                Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService)
-                .logStringMessage('folderAccount - ' + txt);
-            }
-        
-        } catch(e) { }
-      
+      MailServices.mfn.removeListener(folderAccount.folderListener);
     }
 };
 
 
 // Run these when Thunderbird is loaded or unloaded
-
-/* => TB78
-    window.addEventListener("load", function(e) { folderAccount.load(); }, false);
-    window.addEventListener("unload", function(e) { folderAccount.unload(); }, false);
-*/
 
 function onLoad(activatedWhileWindowOpen) {
   folderAccount.load();
