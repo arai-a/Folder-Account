ecffd3db0411fd9bed7547855db049cbf4c58469
Welpy-cw <h.w.forum@arcor.de>
2022-06-18 11:51:40 +0200

    have TB archive messages based on the identity set by FA

--- chrome/content/folderAccount.js
+++ chrome/content/folderAccount.js
@@ -1,4 +1,7 @@
 var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
+var { MailUtils } = ChromeUtils.import("resource:///modules/MailUtils.jsm");
+
+var getIdentityForHeaderOriginal = MailUtils.getIdentityForHeader;
 
 var folderAccount = {
 
@@ -29,23 +32,43 @@ var folderAccount = {
       }
     }
   },
-    
-  // The following is run when Thunderbird is first loaded
+  
+  // hook into function MailUtils.getIdentityForHeader called by MessageArchiver.prototype.archiveMessages,
+  // to provide the identity possibly set for the folder containing the message
+  getIdentityForHeaderOverride: function (hdr, type, hint = "") {
+    try {
+      // check for undefined type to exclude calls from ComposeMessage and SaveAsTemplate in mailCommands.js
+      if (hdr.folder && (type === undefined)) {
+        let identityKey = null;
+        let prefName = "extensions.folderaccount." + hdr.folder.URI;
+        if (Services.prefs.getPrefType(prefName))
+          identityKey = Services.prefs.getCharPref(prefName);
+          if (identityKey)
+            return [MailServices.accounts.getIdentity(identityKey), null];
+      }
+    } catch (e) {
+      console.log("Folder Account: Error in getIdentityForHeaderOverride()")
+    }
+    return getIdentityForHeaderOriginal.apply(this, arguments);
+  },
+
   load: function() {
     // Add a listner to watch for a renamed folder
     // When a folder is renamed, we need to update our stored prefs to reflect the change
     // nsIMsgFolderListener
     MailServices.mfn.addListener(folderAccount.folderListener, MailServices.mfn.folderRenamed);
+    // hook into function called by archiveMessages in MessageArchiver.jsm
+    MailUtils.getIdentityForHeader = folderAccount.getIdentityForHeaderOverride;    
   },
 
-  // The following is run when Thunderbird is unloaded
   unload: function() {
     // Remove our folder listner
     MailServices.mfn.removeListener(folderAccount.folderListener);
+    // restore original function
+    MailUtils.getIdentityForHeader = getIdentityForHeaderOriginal;
   }
+  
 };
-
-// Run these when Thunderbird is loaded or unloaded
 
 function onLoad(activatedWhileWindowOpen) {
   folderAccount.load();
